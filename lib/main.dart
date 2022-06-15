26fb0345ac3283a8baf440213847069e485c2256
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-06-15 09:58:53 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:book_store/views/login_view.dart';
+import 'package:book_store/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashFuturePage(),
+        "/login": (context) => LoginView(),
+      },
+    );
+  }
+}
---
+++ lib/views/splash_view.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:book_store/views/login_view.dart';
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashFuturePage extends StatefulWidget {
+  SplashFuturePage({Key? key}) : super(key: key);
+
+  @override
+  _SplashFuturePageState createState() => _SplashFuturePageState();
+}
+
+class _SplashFuturePageState extends State<SplashFuturePage> {
+  Future<Widget> futureCall() async {
+    // do async operation ( api call, auto login)
+    await Future.delayed(Duration(seconds: 5));
+    return Future.value(LoginView());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return EasySplashScreen(
+      logo: Image.asset("assets/images/logo.png"),
+      logoSize: MediaQuery.of(context).size.width * 0.1,
+      title: Text(
+        "Book Store",
+        style: TextStyle(
+          fontSize: MediaQuery.of(context).size.width * 0.1,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.yellow,
+      futureNavigator: futureCall(),
+    );
+  }
+}
